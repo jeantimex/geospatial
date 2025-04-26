7219d591d503b481d853d5f84fe34846359ffd4a
Yong Su <jean.timex@gmail.com>
2025-04-25 22:51:53 -0700

    created the globe component

--- src/3d-tiles-renderer/index.js
+++ src/3d-tiles-renderer/index.js
@@ -1,164 +1,56 @@
 import './styles.css'; // Import the CSS file
 
 import {
-  GlobeControls,
-  CameraTransitionManager,
-  TilesRenderer,
-} from "3d-tiles-renderer";
-import {
-  TilesFadePlugin,
-  UpdateOnChangePlugin,
-  TileCompressionPlugin,
-  UnloadTilesPlugin,
-  GLTFExtensionsPlugin,
-  BatchedTilesPlugin,
-  GoogleCloudAuthPlugin,
-} from "3d-tiles-renderer/plugins";
-import {
   Scene,
   WebGLRenderer,
   PerspectiveCamera,
-  OrthographicCamera,
 } from "three";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { Globe } from "../components/globe";
 
-let controls, scene, renderer, tiles, transition;
-
-const params = {
-  orthographic: false,
-
-  enableCacheDisplay: false,
-  errorTarget: 40,
-};
-
-init();
-animate();
-
-function reinstantiateTiles() {
-  if (tiles) {
-    scene.remove(tiles.group);
-    tiles.dispose();
-    tiles = null;
-  }
-
-  tiles = new TilesRenderer();
-  tiles.registerPlugin(
-    new GoogleCloudAuthPlugin({
-      apiToken: import.meta.env.VITE_GOOGLE_MAPS_JS_API_KEY,
-      autoRefreshToken: true,
-    })
-  );
-  tiles.registerPlugin(new TileCompressionPlugin());
-  tiles.registerPlugin(new UpdateOnChangePlugin());
-  tiles.registerPlugin(new UnloadTilesPlugin());
-  tiles.registerPlugin(new TilesFadePlugin());
-  tiles.registerPlugin(
-    new GLTFExtensionsPlugin({
-      // Using local files from public directory for DRACO decoder
-      dracoLoader: new DRACOLoader().setDecoderPath("/assets/draco/"),
-    })
-  );
-
-  if (params.useBatchedMesh) {
-    tiles.registerPlugin(
-      new BatchedTilesPlugin({
-        renderer,
-        discardOriginalContent: false,
-        instanceCount: 250,
-      })
-    );
-  }
-
-  tiles.group.rotation.x = -Math.PI / 2;
-  scene.add(tiles.group);
-
-  tiles.setResolutionFromRenderer(transition.camera, renderer);
-  tiles.setCamera(transition.camera);
-
-  controls.setTilesRenderer(tiles);
-}
+let globe, scene, renderer, camera;
 
 function init() {
-  // renderer
-  renderer = new WebGLRenderer({ antialias: true });
-  renderer.setClearColor(0x151c1f);
-  document.body.appendChild(renderer.domElement);
-
   // scene
   scene = new Scene();
 
-  // camera and transition set up
-  transition = new CameraTransitionManager(
-    new PerspectiveCamera(
-      60,
-      window.innerWidth / window.innerHeight,
-      1,
-      160000000
-    ),
-    new OrthographicCamera(-1, 1, 1, -1, 1, 160000000)
-  );
-  transition.perspectiveCamera.position.set(4800000, 2570000, 14720000);
-  transition.perspectiveCamera.lookAt(0, 0, 0);
-  transition.autoSync = false;
-
-  transition.addEventListener("camera-change", ({ camera, prevCamera }) => {
-    tiles.deleteCamera(prevCamera);
-    tiles.setCamera(camera);
-    controls.setCamera(camera);
+  // renderer
+  renderer = new WebGLRenderer({
+    antialias: true,
+    depth: true,
+    logarithmicDepthBuffer: true,
   });
+  document.body.appendChild(renderer.domElement);
 
-  // disable adjusting the orthographic camera position for zoom since globe controls will do this
-  transition.orthographicPositionalZoom = false;
+  // camera
+  const aspect = window.innerWidth / window.innerHeight
+  camera = new PerspectiveCamera(75, aspect, 10, 1e6);
+  camera.position.set(4800000, 2570000, 14720000);
+  camera.lookAt(0, 0, 0);
 
-  // controls
-  controls = new GlobeControls(
-    scene,
-    transition.camera,
-    renderer.domElement,
-    null
-  );
-  controls.enableDamping = true;
+  globe = new Globe(scene, camera, renderer);
 
-  // initialize tiles
-  reinstantiateTiles();
-
+  window.addEventListener("resize", onWindowResize);
   onWindowResize();
-  window.addEventListener("resize", onWindowResize, false);
+
+  animate();
 }
 
 function onWindowResize() {
-  const { perspectiveCamera, orthographicCamera } = transition;
-  const aspect = window.innerWidth / window.innerHeight;
+  // camera
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-  perspectiveCamera.aspect = aspect;
-  perspectiveCamera.updateProjectionMatrix();
-
-  orthographicCamera.left = -orthographicCamera.top * aspect;
-  orthographicCamera.right = -orthographicCamera.left;
-  orthographicCamera.updateProjectionMatrix();
-
+  // renderer
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
 }
 
 function animate() {
-  requestAnimationFrame(animate);
-
-  if (!tiles) return;
-
-  controls.enabled = !transition.animating;
-  controls.update();
-  transition.update();
-
-  // update options
-  const camera = transition.camera;
-  tiles.setResolutionFromRenderer(camera, renderer);
-  tiles.setCamera(camera);
-
-  // update tiles
   camera.updateMatrixWorld();
-  tiles.errorTarget = params.errorTarget;
-  tiles.update();
-
+  globe.update();
   renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
 }
+
+window.addEventListener('load', init);
---
+++ src/components/globe.js
@@ -0,0 +1,64 @@
+import {
+  GlobeControls,
+  TilesRenderer,
+} from "3d-tiles-renderer";
+import {
+  TilesFadePlugin,
+  UpdateOnChangePlugin,
+  TileCompressionPlugin,
+  UnloadTilesPlugin,
+  GLTFExtensionsPlugin,
+  GoogleCloudAuthPlugin,
+} from "3d-tiles-renderer/plugins";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+export class Globe {
+  constructor(scene, camera, renderer) {
+    this.scene = scene;
+    this.camera = camera;
+    this.renderer = renderer;
+
+    this.tiles = new TilesRenderer();
+    this.tiles.registerPlugin(
+      new GoogleCloudAuthPlugin({
+        apiToken: import.meta.env.VITE_GOOGLE_MAPS_JS_API_KEY,
+        autoRefreshToken: true,
+      })
+    );
+    this.tiles.registerPlugin(new TileCompressionPlugin());
+    this.tiles.registerPlugin(new UpdateOnChangePlugin());
+    this.tiles.registerPlugin(new UnloadTilesPlugin());
+    this.tiles.registerPlugin(new TilesFadePlugin());
+    this.tiles.registerPlugin(
+      new GLTFExtensionsPlugin({
+        // Using local files from public directory for DRACO decoder
+        dracoLoader: new DRACOLoader().setDecoderPath("/assets/draco/"),
+      })
+    );
+  
+    this.tiles.group.rotation.x = -Math.PI / 2;
+    this.scene.add(this.tiles.group);
+  
+    this.tiles.setResolutionFromRenderer(this.camera, this.renderer);
+    this.tiles.setCamera(this.camera);
+
+    this.controls = new GlobeControls(
+      this.scene,
+      this.camera,
+      this.renderer.domElement,
+      this.tiles,
+    );
+    this.controls.enableDamping = true;
+  }
+
+  update() {
+    this.controls.enabled = true;
+    this.controls.update();
+
+    // update options
+    this.tiles.setResolutionFromRenderer(this.camera, this.renderer);
+    this.tiles.setCamera(this.camera);
+
+    this.tiles.update();
+  }
+}
