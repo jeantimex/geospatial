ca8c774b0035220b3b78ef916a201d94d6c1af36
Yong Su <jean.timex@gmail.com>
2025-05-04 21:15:18 -0700

    fixed the clouds demo

--- src/clouds/index.ts
+++ src/clouds/index.ts
@@ -52,7 +52,6 @@ import {
 import {
   DitheringEffect,
   LensFlareEffect,
-  NormalEffect
 } from '@takram/three-geospatial-effects'
 
 let renderer: WebGLRenderer;
@@ -144,7 +143,6 @@ function init(): void {
   });
   composer.addPass(new RenderPass(scene, camera));
   composer.addPass(normalPass);
-  composer.addPass(new EffectPass(camera, new NormalEffect(camera)));
   composer.addPass(new EffectPass(camera, clouds, aerialPerspective));
   composer.addPass(
     new EffectPass(
